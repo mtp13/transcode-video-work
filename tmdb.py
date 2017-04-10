3b64c7963be7f8cbb6dd37b4faa2da83b8346257
Mike Pullen <mike.pullen@gmail.com>
2017-04-09 20:32:01 -0500

    Refactor (pylint messages)

--- tmdb.py
+++ tmdb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+"Get the DVD title from Safari and save to the queue file."
 # Anchorman The Legend of Ron Burgundy (2004) - The Movie Database (TMDb)
 #
 # use applescript to get the TMDb webpage title for the movie that
@@ -11,43 +11,45 @@
 # write the new dvd_title to the file
 # and copy it to the clipboard
 
-from subprocess import Popen, PIPE
-from unidecode import unidecode
 import re
 import fileinput
+from subprocess import Popen, PIPE
+from unidecode import unidecode
 
 
-def run_this_ascript(ascript, args=[]):
+def run_this_ascript(ascript, args=None):
     "Run the given AppleScript and return the standard output."
-    p = Popen(['osascript', '-'] + args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = p.communicate(ascript)
+    args = args or []
+    pipe = Popen(['osascript', '-'] + args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = pipe.communicate(ascript)
     return stdout
 
 
-def setClipboardData(data):
-    p = Popen(['pbcopy'], stdin=PIPE)
-    p.communicate(data)
+def set_clipboard_data(data):
+    "Copy the data to the system clipboard."
+    pipe = Popen(['pbcopy'], stdin=PIPE)
+    pipe.communicate(data)
 
 
 QUEUE = '/Volumes/Media Disk/transcode-video-work/queue.txt'
 DVD_FOLDER = '/Volumes/Media Disk/DVD Backups/'
 
-ascript = """
+ASCRIPT = """
 tell application "Safari"
 set theTitle to the name of the front document
 end tell
 """
-dvd_title = run_this_ascript(ascript)
-dvd_title = unidecode(unicode(dvd_title, 'utf-8'))
+DVD_TITLE = run_this_ascript(ASCRIPT)
+DVD_TITLE = unidecode(unicode(DVD_TITLE, 'utf-8'))
 
-dvd_title = re.sub('[^a-zA-Z0-9()! \-]', '', dvd_title)
-dvd_title = re.sub(' -- .*', '', dvd_title)
+DVD_TITLE = re.sub(r'[^a-zA-Z0-9()! \-]', '', DVD_TITLE)
+DVD_TITLE = re.sub(r' -- .*', '', DVD_TITLE)
 
 for line in fileinput.input(QUEUE, inplace=1):
-    if re.search('\S', line):
+    if re.search(r'\S', line):
         print line.strip()
 with open(QUEUE, 'a') as q:
-    q.write(DVD_FOLDER + dvd_title + '.mkv\n')
-print DVD_FOLDER + dvd_title + '.mkv'
+    q.write(DVD_FOLDER + DVD_TITLE + '.mkv\n')
+print DVD_FOLDER + DVD_TITLE + '.mkv'
 
-setClipboardData(dvd_title)
+set_clipboard_data(DVD_TITLE)
